16286433f8c1be17b6fffbaff65f1c7023e5acef
Ernest Zet <ernest52@onet.pl>
2024-08-28 14:08:32 +0200

    adding loadingComponent layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoadingPageComponent } from './loading-page/loading-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoadingPageComponent },
+  { path: '**', redirectTo: '/' },
+];
